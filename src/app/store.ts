93244c17a790cb735fb2623bb964aa084d659c21
EgoshinVA <egoshinvit@gmail.com>
2025-01-26 22:27:56 +0700

    17 todolist for 1/2

---
+++ src/app/store.ts
@@ -0,0 +1,28 @@
+import { combineReducers, UnknownAction } from "redux"
+import { ThunkDispatch } from "redux-thunk"
+import { tasksReducer } from "../features/todolists/model/tasks-reducer"
+import { todolistsReducer } from "../features/todolists/model/todolists-reducer"
+import { configureStore } from "@reduxjs/toolkit"
+import authReducer from "../features/auth/model/auth-reducer"
+import appReducer from "./app-reducer"
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+  app: appReducer,
+  auth: authReducer,
+})
+
+export const store = configureStore({
+  reducer: rootReducer,
+})
+
+export type RootState = ReturnType<typeof store.getState>
+
+// export type AppDispatch = typeof store.dispatch
+
+// Создаем тип диспатча который принимает как AC так и TC
+export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>
+
+// @ts-ignore
+window.store = store
